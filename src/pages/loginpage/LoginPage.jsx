8ab385f9b72688ca5774e43d4c032d3d566f2e47
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2022-08-08 23:10:51 +0500

    Updated styles and pages

--- src/pages/loginpage/LoginPage.jsx
+++ src/pages/loginpage/LoginPage.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './styles.LoginPage'
 
 const LoginPage = () => {
     return (
         <div className='LoginPage'>
             <h1>This is login page!</h1>
-            <Link to='/main'>
-                <button>Log In!</button>
-            </Link>
+            <div>
+                <input type="email" id="email" />
+            </div>
+            <div>
+                <input type="password" id="password" />
+            </div>
+            <div>
+                <Link to='/main'>
+                    <button>Log In!</button>
+                </Link>
+            </div>
         </div>
     );
 };
--- src/pages/page404/Page404.jsx
+++ src/pages/page404/Page404.jsx
@@ -1,12 +1,13 @@
+import './styles.Page404'
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 
 const Page404 = () => {
     return (
-        <div className='404'>
-            <h1>Страница не найдена!</h1>
-            <p>Но Вы всегда можете вернуться <Link to='main'>домой</Link>  :)</p>
+        <div className='Page404'>
+                <h1>Страница не найдена!</h1>
+                <p>Но Вы всегда можете вернуться <Link to='main'>домой</Link>  :)</p>
         </div>
     );
 };
