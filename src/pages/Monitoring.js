2141c75bfd350c440d0acd32f1fd71bdfb5f210f
i21s692 <i21s692@students.it-college.ru>
2022-08-25 00:08:22 +0500

    add_cards

--- src/pages/Monitoring.js
+++ src/pages/Monitoring.js
@@ -1,32 +1,46 @@
 // @mui
-import { Container, Divider, Typography, Stack, Button, Grid } from '@mui/material';
+import {
+  Container,
+  Divider,
+  Typography,
+  Stack,
+  Button,
+  Grid,
+} from "@mui/material";
 // hooks
-import useSettings from '../hooks/useSettings';
+import useSettings from "../hooks/useSettings";
 // components
-import Page from '../components/Page';
+import Page from "../components/Page";
 // sections
 import {
   AnalyticsWidgetSummary,
   TotalOfCharges,
   InfoSession,
   BankingBalanceStatistics,
-  AppNewInvoice,
   MonitoringStantionsComponent1,
   MonitoringStantionsComponent2,
   MonitoringStateComponent,
-} from '../sections/monitoring';
+  NetworkStatus,
+} from "../sections/monitoring";
 
 // ----------------------------------------------------------------------
 
 export default function Monitoring() {
   const { themeStretch } = useSettings();
   const current = new Date();
-  const date = `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()}`;
+  const date = `${current.getDate()}/${
+    current.getMonth() + 1
+  }/${current.getFullYear()}`;
 
   return (
     <Page title="Мониторинг">
-      <Container maxWidth={themeStretch ? false : 'xl'}>
-        <Stack direction={{ xs: 'column', sm: 'row' }} justifyContent="space-between" alignItems="baseline" spacing={2}>
+      <Container maxWidth={themeStretch ? false : "xl"}>
+        <Stack
+          direction={{ xs: "column", sm: "row" }}
+          justifyContent="space-between"
+          alignItems="baseline"
+          spacing={2}
+        >
           <Typography variant="h3" component="h1" paragraph>
             Мониторинг
           </Typography>
@@ -37,21 +51,29 @@ export default function Monitoring() {
             </Button>
           </Typography>
         </Stack>
-        <Divider sx={{ bgcolor: 'black', mb: 2 }} />
+        <Divider sx={{ bgcolor: "black", mb: 2 }} />
 
         <Grid container spacing={3}>
-
           <Grid item xs={12} md={6} lg={8}>
             <TotalOfCharges />
           </Grid>
           <Grid item xs={12} md={6} lg={4}>
             <Stack spacing={2}>
               <MonitoringStantionsComponent1 />
-              <MonitoringStantionsComponent2/>
+              <MonitoringStantionsComponent2 />
             </Stack>
           </Grid>
-          <Grid item xs={12} lg={8}>
-            <AppNewInvoice />
+          <Grid item xs={3} lg={2}>
+            <NetworkStatus />
+          </Grid>
+          <Grid item xs={3} lg={2}>
+            <NetworkStatus />
+          </Grid>
+          <Grid item xs={3} lg={2}>
+            <NetworkStatus />
+          </Grid>
+          <Grid item xs={3} lg={2}>
+            <NetworkStatus />
           </Grid>
           <Grid item xs={12} md={12} lg={4}>
             <MonitoringStateComponent />
---
+++ src/sections/monitoring/NetworkStatus.js
@@ -0,0 +1,67 @@
+import merge from 'lodash/merge';
+import ReactApexChart from 'react-apexcharts';
+// @mui
+import { useTheme } from '@mui/material/styles';
+import { Card, Typography, Stack, Divider } from '@mui/material';
+// utils
+import { fNumber } from '../../utils/formatNumber';
+//
+import { BaseOptionChart } from '../../components/chart';
+
+// ----------------------------------------------------------------------
+
+const CHART_SIZE = { width: 106, height: 106 };
+const TOTAL_CHECK_OUT = 18472;
+const CHART_DATA_CHECK_OUT = [64];
+
+export default function NetworkStatus() {
+  const theme = useTheme();
+
+  const chartOptionsCheckIn = merge(BaseOptionChart(), {
+    chart: { sparkline: { enabled: true } },
+    grid: {
+      padding: {
+        top: -9,
+        bottom: -9,
+      },
+    },
+    legend: { show: false },
+    plotOptions: {
+      radialBar: {
+        hollow: { size: '64%' },
+        track: { margin: 0 },
+        dataLabels: {
+          name: { show: false },
+          value: {
+            offsetY: 6,
+            fontSize: theme.typography.subtitle2.fontSize,
+          },
+        },
+      },
+    },
+  });
+
+  const chartOptionsCheckOut = {
+    ...chartOptionsCheckIn,
+    colors: [theme.palette.chart.yellow[0]],
+  };
+
+  return (
+    <Card sx={{display:'flex', flexDirection:'column', alignItems:'center', py:5}}>
+          <ReactApexChart
+            type="radialBar"
+            series={CHART_DATA_CHECK_OUT}
+            options={chartOptionsCheckOut}
+            {...CHART_SIZE}
+          />
+          <div>
+            <Typography variant="h4" textAlign={'center'}>
+              {fNumber(TOTAL_CHECK_OUT)}
+            </Typography>
+            <Typography variant="body2" textAlign={'center'}>
+              Число занятых станций
+            </Typography>
+          </div>
+    </Card>
+  );
+}
