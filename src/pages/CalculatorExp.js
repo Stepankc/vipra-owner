502bdd60a83988d2c54f75db766d122ba0277423
i21s692 <i21s692@students.it-college.ru>
2022-08-27 16:17:22 +0500

    add_page_calculatorExp

--- src/pages/CalculatorExp.js
+++ src/pages/CalculatorExp.js
@@ -1,14 +1,37 @@
+import * as React from "react";
 // @mui
-import { Container, Typography, Divider } from "@mui/material";
+import {
+  Container,
+  Typography,
+  Divider,
+  Button,
+  Grid,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Stack,
+} from "@mui/material";
 // hooks
 import useSettings from "../hooks/useSettings";
 // components
 import Page from "../components/Page";
 
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+
+import TotalCard from "../sections/calculatorExp/TotalCard";
+import ExpenseTable from "../sections/calculatorExp/ExpenseTable"
+import IncomeExpenseChart from "../sections/calculatorExp/IncomeExpenseChart"
+
 // ----------------------------------------------------------------------
 
 export default function PageTwo() {
   const { themeStretch } = useSettings();
+
+  const [firstDate, setFirstDate] = React.useState(null);
 
   return (
     <Page title="Калькулятор расходов">
@@ -17,26 +40,60 @@ export default function PageTwo() {
           Калькулятор расходов
         </Typography>
         <Divider sx={{ bgcolor: "black", mb: 2 }} />
-        <Typography gutterBottom>
-          Curabitur turpis. Vestibulum facilisis, purus nec pulvinar iaculis,
-          ligula mi congue nunc, vitae euismod ligula urna in dolor. Nam quam
-          nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Phasellus
-          blandit leo ut odio. Vestibulum ante ipsum primis in faucibus orci
-          luctus et ultrices posuere cubilia Curae; Fusce id purus. Aliquam
-          lorem ante, dapibus in, viverra quis, feugiat a, tellus. In
-          consectetuer turpis ut velit. Aenean posuere, tortor sed cursus
-          feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis
-          lacus. Vestibulum suscipit nulla quis orci. Nam commodo suscipit quam.
-          Sed a libero.
-        </Typography>
-        <Typography>
-          Praesent ac sem eget est egestas volutpat. Phasellus viverra nulla ut
-          metus varius laoreet. Curabitur ullamcorper ultricies nisi. Ut non
-          enim eleifend felis pretium feugiat. Donec mi odio, faucibus at,
-          scelerisque quis, convallis in, nisi. Fusce vel dui. Quisque libero
-          metus, condimentum nec, tempor a, commodo mollis, magna. In enim
-          justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Cras dapibus.
-        </Typography>
+        <Grid container spacing={2} >
+          {/* left side */}
+          <Grid item xs={12} md={7} lg={7}>
+            <Grid container gap={2}>
+              <Grid item xs={12} md={12} lg={12}>
+                <Button variant="contained">Добавить Расходы</Button>
+              </Grid>
+              <Grid item xs={12} md={3.5} lg={3.5}>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <DatePicker
+                    label="Период"
+                    value={firstDate}
+                    onChange={(newValue) => {
+                      setFirstDate(newValue);
+                    }}
+                    renderInput={(params) => <TextField {...params} sx={{width: '100%'}} />}
+                  />
+                </LocalizationProvider>
+              </Grid>
+              <Grid item xs={12} md={3.5} lg={3.5}>
+                <TextField variant="outlined" label="Название" fullWidth />
+              </Grid>
+              <Grid item xs={12} md={3.5} lg={3.5}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">Роль</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    label="Age"
+                    defaultValue=""
+                  >
+                    <MenuItem value={1}>Энергозатраты</MenuItem>
+                    <MenuItem value={2}>Аренда</MenuItem>
+                    <MenuItem value={3}>Ремон</MenuItem>
+                    <MenuItem value={4}>Обслуживание</MenuItem>
+                    <MenuItem value={5}>Бесплатные зарядки</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={12} lg={12}>
+                <ExpenseTable />
+              </Grid>
+              <Grid item xs={12} md={12} lg={12}>
+                <IncomeExpenseChart />
+              </Grid>
+            </Grid>
+          </Grid>
+          {/* left side end */}
+          {/* right side */}
+          <Grid item xs={12} md={5} lg={5}>
+            <TotalCard />
+          </Grid>
+          {/* right side end */}
+        </Grid>
       </Container>
     </Page>
   );
