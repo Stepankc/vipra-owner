cde901154ed2f1b6c3d23d58a824b829f0c1a1dd
i21s692 <i21s692@students.it-college.ru>
2022-08-28 12:38:34 +0500

    component_for_calculatorExp

--- src/sections/calculatorExp/TotalCard.js
+++ src/sections/calculatorExp/TotalCard.js
@@ -1,12 +1,25 @@
 // @mui
-import { Card, Container, Typography } from '@mui/material';
+import { Card, Grid, Typography } from '@mui/material';
 // ----------------------------------------------------------------------
 
 export default function TotalCard() {
 
   return (
     <Card sx={{p:2}}>
+      <Grid container  rowGap={5} spacing={2}>
+        <Grid item display={'flex'} alignItems={'center'} flexDirection={'column'} xs={6} md={6} lg={6}>
         <Typography variant='h6' component='h1'>Итого доходов:</Typography>
+        <Typography variant='h6' component='h1'>222 руб.</Typography>
+        </Grid>
+        <Grid item display={'flex'} alignItems={'center'} flexDirection={'column'} xs={6} md={6} lg={6}>
+        <Typography variant='h6' component='h1'>Итого расходов:</Typography>
+        <Typography variant='h6' component='h1'>333 руб.</Typography>
+        </Grid>
+        <Grid item display={'flex'} alignItems={'center'} flexDirection={'column'} xs={6} md={6} lg={6}>
+        <Typography variant='h6' component='h1'>Итого:</Typography>
+        <Typography variant='h6' component='h1'>22132 руб.</Typography>
+        </Grid>
+      </Grid>
     </Card>
   );
 }
