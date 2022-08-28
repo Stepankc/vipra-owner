dc1c48db5c9287b64ae6587d0a49c723298a7399
i21s692 <i21s692@students.it-college.ru>
2022-08-29 01:21:12 +0500

    add_mock_on_card

--- src/sections/calculatorExp/TotalCard.js
+++ src/sections/calculatorExp/TotalCard.js
@@ -2,22 +2,23 @@
 import { Card, Grid, Typography } from '@mui/material';
 // ----------------------------------------------------------------------
 
-export default function TotalCard() {
+export default function TotalCard(props) {
 
+  console.log(props.total)
   return (
     <Card sx={{p:2}}>
       <Grid container  rowGap={5} spacing={2}>
         <Grid item display={'flex'} alignItems={'center'} flexDirection={'column'} xs={6} md={6} lg={6}>
         <Typography variant='h6' component='h1'>Итого доходов:</Typography>
-        <Typography variant='h6' component='h1'>222 руб.</Typography>
+        <Typography variant='h6' component='h1'>{props.total.income} руб.</Typography>
         </Grid>
         <Grid item display={'flex'} alignItems={'center'} flexDirection={'column'} xs={6} md={6} lg={6}>
         <Typography variant='h6' component='h1'>Итого расходов:</Typography>
-        <Typography variant='h6' component='h1'>333 руб.</Typography>
+        <Typography variant='h6' component='h1'>{props.total.expense} руб.</Typography>
         </Grid>
         <Grid item display={'flex'} alignItems={'center'} flexDirection={'column'} xs={6} md={6} lg={6}>
         <Typography variant='h6' component='h1'>Итого:</Typography>
-        <Typography variant='h6' component='h1'>22132 руб.</Typography>
+        <Typography variant='h6' component='h1'>{props.total.income-props.total.expense} руб.</Typography>
         </Grid>
       </Grid>
     </Card>
