68a0a7a0091bf4b5f9350c8d9eba6da738e298ad
i21s692 <i21s692@students.it-college.ru>
2022-08-29 14:18:41 +0500

    components_table

---
+++ src/sections/MyStation/StationTable.js
@@ -0,0 +1,223 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+// @mui
+import {
+  Box,
+  Card,
+  Table,
+  Tooltip,
+  TableBody,
+  IconButton,
+  TableContainer,
+  TablePagination,
+} from '@mui/material';
+// routes
+import { PATH_DASHBOARD } from '../../routes/paths';
+// hooks
+import useTabs from '../../hooks/useTabs';
+import useTable, { getComparator, emptyRows } from '../../hooks/useTable';
+// _mock_
+import { _reportList } from '../../_mock';
+// components
+import Iconify from '../../components/Iconify';
+import Scrollbar from '../../components/Scrollbar';
+import { TableEmptyRows, TableHeadCustom, TableNoData, TableSelectedActions } from '../../components/table';
+// sections
+import StationTableRow from './StationTableRow'
+
+// ----------------------------------------------------------------------
+
+const TABLE_HEAD = [
+  { id: 'nameStation', label: 'Название станции', align: 'left' },
+  { id: 'name', label: 'Владелец', align: 'left' },
+  { id: 'status', label: 'Статус', align: 'left' },
+  { id: 'price', label: 'Совершено зарядок', align: 'center', width: 140 },
+  { id: 'sent', label: 'Заработано на зарядках', align: 'center', width: 140 },
+  { id: '' },
+];
+
+// ----------------------------------------------------------------------
+
+export default function InvoiceList() {
+
+  const navigate = useNavigate();
+
+  const {
+    dense,
+    page,
+    order,
+    orderBy,
+    rowsPerPage,
+    //
+    selected,
+    setSelected,
+    onSelectRow,
+    onSelectAllRows,
+    //
+    onSort,
+    onChangePage,
+    onChangeRowsPerPage,
+  } = useTable({ defaultOrderBy: 'createDate' });
+
+  const [tableData, setTableData] = useState(_reportList);
+
+
+  const [filterService] = useState('all');
+
+  const { currentTab: filterStatus } = useTabs('all');
+
+
+
+  const handleDeleteRow = (id) => {
+    const deleteRow = tableData.filter((row) => row.id !== id);
+    setSelected([]);
+    setTableData(deleteRow);
+  };
+
+  const handleDeleteRows = (selected) => {
+    const deleteRows = tableData.filter((row) => !selected.includes(row.id));
+    setSelected([]);
+    setTableData(deleteRows);
+  };
+
+  const handleEditRow = (id) => {
+    navigate(PATH_DASHBOARD.invoice.edit(id));
+  };
+
+  const handleViewRow = (id) => {
+    navigate(PATH_DASHBOARD.invoice.view(id));
+  };
+
+  const dataFiltered = applySortFilter({
+    tableData,
+    comparator: getComparator(order, orderBy),
+    filterService,
+    filterStatus,
+  });
+
+  const denseHeight = dense ? 56 : 76;
+
+  const isNotFound =
+    (!dataFiltered.length && !!filterStatus) ||
+    (!dataFiltered.length && !!filterService);
+
+
+  return (
+        <Card>
+          <Scrollbar>
+            <TableContainer sx={{ minWidth: 800, position: 'relative' }}>
+              {selected.length > 0 && (
+                <TableSelectedActions
+                  dense={dense}
+                  numSelected={selected.length}
+                  rowCount={tableData.length}
+                  onSelectAllRows={(checked) =>
+                    onSelectAllRows(
+                      checked,
+                      tableData.map((row) => row.id)
+                    )
+                  }
+                  actions={
+
+                      <Tooltip title="Delete">
+                        <IconButton color="primary" onClick={() => handleDeleteRows(selected)}>
+                          <Iconify icon={'eva:trash-2-outline'} />
+                        </IconButton>
+                      </Tooltip>
+                  }
+                />
+              )}
+
+              <Table size={'medium'}>
+                <TableHeadCustom
+                  order={order}
+                  orderBy={orderBy}
+                  headLabel={TABLE_HEAD}
+                  rowCount={tableData.length}
+                  numSelected={selected.length}
+                  onSort={onSort}
+                />
+
+                <TableBody>
+                  {dataFiltered.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
+                    <StationTableRow
+                      key={row.id}
+                      row={row}
+                      selected={selected.includes(row.id)}
+                      onSelectRow={() => onSelectRow(row.id)}
+                      onViewRow={() => handleViewRow(row.id)}
+                      onEditRow={() => handleEditRow(row.id)}
+                      onDeleteRow={() => handleDeleteRow(row.id)}
+                    />
+                  ))}
+
+                  <TableEmptyRows height={denseHeight} emptyRows={emptyRows(page, rowsPerPage, tableData.length)} />
+
+                  <TableNoData isNotFound={isNotFound} />
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Scrollbar>
+
+          <Box sx={{ position: 'relative' }}>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25]}
+              labelRowsPerPage={"показывать"}
+              component="div"
+              count={dataFiltered.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={onChangePage}
+              onRowsPerPageChange={onChangeRowsPerPage}
+            />
+          </Box>
+        </Card>
+  );
+}
+
+// ----------------------------------------------------------------------
+
+function applySortFilter({
+  tableData,
+  comparator,
+  filterName,
+  filterStatus,
+  filterService,
+  filterStartDate,
+  filterEndDate,
+}) {
+  const stabilizedThis = tableData.map((el, index) => [el, index]);
+
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+
+  tableData = stabilizedThis.map((el) => el[0]);
+
+  if (filterName) {
+    tableData = tableData.filter(
+      (item) =>
+        item.invoiceNumber.toLowerCase().indexOf(filterName.toLowerCase()) !== -1 ||
+        item.invoiceTo.name.toLowerCase().indexOf(filterName.toLowerCase()) !== -1
+    );
+  }
+
+  if (filterStatus !== 'all') {
+    tableData = tableData.filter((item) => item.status === filterStatus);
+  }
+
+  if (filterService !== 'all') {
+    tableData = tableData.filter((item) => item.items.some((c) => c.service === filterService));
+  }
+
+  if (filterStartDate && filterEndDate) {
+    tableData = tableData.filter(
+      (item) =>
+        item.createDate.getTime() >= filterStartDate.getTime() && item.createDate.getTime() <= filterEndDate.getTime()
+    );
+  }
+
+  return tableData;
+}
---
+++ src/sections/MyStation/StationTableRow.js
@@ -0,0 +1,41 @@
+import PropTypes from "prop-types";
+// @mui
+import { TableRow, TableCell } from "@mui/material";
+
+// ----------------------------------------------------------------------
+
+InvoiceTableRow.propTypes = {
+  row: PropTypes.object.isRequired,
+  selected: PropTypes.bool,
+  onSelectRow: PropTypes.func,
+  onViewRow: PropTypes.func,
+  onDeleteRow: PropTypes.func,
+};
+
+export default function InvoiceTableRow({ row, selected }) {
+  const { nameStation, name, status, sumCharge, earnMoney } = row;
+
+  return (
+    <TableRow hover selected={selected}>
+      <TableCell align="left" sx={{ textTransform: "capitalize" }}>
+        {nameStation}
+      </TableCell>
+
+      <TableCell align="left" sx={{ textTransform: "capitalize" }}>
+        {name}
+      </TableCell>
+
+      <TableCell align="left" sx={{ textTransform: "capitalize" }}>
+        {status}
+      </TableCell>
+
+      <TableCell align="center" sx={{ textTransform: "capitalize" }}>
+        {sumCharge}
+      </TableCell>
+
+      <TableCell align="center" sx={{ textTransform: "capitalize" }}>
+        {earnMoney} руб.
+      </TableCell>
+    </TableRow>
+  );
+}
