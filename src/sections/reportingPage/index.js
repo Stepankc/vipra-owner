ac64517eb3aba4c671006f143e4d7250f34e10d1
i21s692 <i21s692@students.it-college.ru>
2022-08-29 13:57:06 +0500

    change_import

--- src/sections/reportingPage/index.js
+++ src/sections/reportingPage/index.js
@@ -2,3 +2,4 @@ export { default as CountEnergy } from './countEnergy';
 export { default as CountMoney } from './countMoney';
 export { default as CountSession } from './countSession';
 export { default as InfoConnect } from './infoConnect';
+export { default as CheckStats } from './CheckStats';
